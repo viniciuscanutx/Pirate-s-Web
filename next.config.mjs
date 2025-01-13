84d0ef62952bb6b3100745d32c34cd7af3e7f104
viniciusucanutx <viniciuscanutmz@gmail.com>
2025-01-13 13:01:20 -0300

    :heavy_plus_sign: build: Componentes do shadcn/ui para estilização.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'm.media-amazon.com',
+      'web-films-api.vercel.app',
+      'warezcdn.link', 
+      'via.placeholder.com',
+      'www.justwatch.com',
+      'images.plex.tv',
+      'i.pinimg.com',
+      'resizing.flixster.com',
+      'image.tmdb.org',
+      'media.themoviedb.org'
+    ],
+  },
 };
 
 export default nextConfig;
