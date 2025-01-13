7150121e649af80b905ddcfbd4f396e6a11b9aa0
viniciusucanutx <viniciuscanutmz@gmail.com>
2025-01-13 16:13:46 -0300

    :sparkles: feat: Páginas de Login/Perfil com favoritos.

---
+++ pages/Favorites.jsx
@@ -0,0 +1,193 @@
+import { useRouter } from "next/router";
+import React, { useState, useEffect } from "react";
+
+import { ref, onValue } from "firebase/database";
+import { db } from "@/config/firebaseConfig";
+import { getAuth, signOut } from "firebase/auth";
+
+const { fetchNMovies, fetchNSeries } = require('@/pages/api/client');
+
+import Navbar from "@/components/Navbar/navbar";
+import MovieRow from "@/components/MovieRow/movie-row";
+
+const Favorites = () => {
+    const [likedContent, setLikedContent] = useState([]);
+    const [user, setUser ] = useState(null);
+    const [allContent, setAllContent] = useState([]);
+    const [allSeries, setAllSeries] = useState([]); 
+    const [loading, setLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        const auth = getAuth();
+        const unsubscribeAuth = auth.onAuthStateChanged((currentUser ) => {
+            if (currentUser ) {
+                setUser (currentUser );
+            } else {
+                router.push("/Login");
+            }
+        });
+
+        return () => unsubscribeAuth();
+    }, [router]);
+
+    useEffect(() => {
+        if (!user) return;
+
+        const likedContentRef = ref(db, `like`);
+        const unsubscribe = onValue(likedContentRef, (snapshot) => {
+            const data = snapshot.val();
+            if (data) {
+                const userLikedContent = Object.entries(data).flatMap(([contentId, users]) =>
+                    users[user.uid] ? [contentId] : []
+                );
+                setLikedContent(userLikedContent);
+            } else {
+                setLikedContent([]);
+            }
+        });
+
+        return () => unsubscribe();
+    }, [user]);
+
+    useEffect(() => {
+        const fetchContent = async () => {
+            try {
+                const data = await fetchNMovies()
+
+                const allMovies = data.map((movie) => ({
+                    id: movie._id,
+                    imdbid: movie.imdbid,
+                    titulo: movie.titulo,
+                    sinopse: movie.sinopse,
+                    duracao: movie.duracao,
+                    lancamento: movie.lancamento,
+                    genero: movie.genero,
+                    diretor: movie.diretor,
+                    atores: movie.atores,
+                    renda: movie.renda,
+                    nota: movie.nota,
+                    metacritic: movie.metacritic,
+                    rottentomatoes: movie.rottentomatoes,
+                    poster: movie.poster,
+                    link: movie.link,
+                    legenda: movie.legenda,
+                    banner: movie.banner,
+                    fullRelease: movie.fulllancamento,
+                    hasSubtitles: !!movie.legenda,
+                    isDubbed: false,
+                }));
+                setAllContent(allMovies);
+                setLoading(false);
+            } catch (error) {
+                console.error("Erro ao buscar conteúdo:", error);
+                setLoading(false);
+            }
+        };
+
+        fetchContent();
+    }, []);
+
+    const favoriteContent = allContent.filter((item) => likedContent.includes(item.id));
+
+    useEffect(() => {
+        const fetchSeries = async () => {
+            try {
+                const data = await fetchNSeries()
+
+                const allSeries = data.map((series) => ({
+                    id: series._id,
+                    imdbid: series.imdbid,
+                    titulo: series.titulo,
+                    sinopse: series.sinopse,
+                    lancamento: series.lancamento,
+                    genero: series.genero,
+                    nota: series.avaliacao,
+                    parentalRating: series.parentalRating,
+                    poster: series.poster,
+                    banner: series.banner,
+                    hasSubtitles: !!series.temporadas.some(temporada => temporada.episodios.length > 0),
+                    isDubbed: false,
+                    seasons: series.temporadas.map(temporada => ({
+                        number: temporada.numero,
+                        episodes: temporada.episodios.map(episodio => ({
+                            number: episodio.numero,
+                            title: episodio.titulo,
+                            link: episodio.link,
+                            subtitles: episodio.legenda,
+                        })),
+                    })),
+                }));
+                setAllSeries(allSeries);
+            } catch (error) {
+                console.error("Erro ao buscar séries:", error);
+            }
+        };
+
+        fetchSeries();
+    }, []);
+
+    const favoriteSeries = allSeries.filter((item ) => likedContent.includes(item.id));
+
+    const handleLogout = () => {
+        const auth = getAuth();
+        signOut(auth)
+            .then(() => {
+                console.log("Usuário deslogado");
+                router.push("/");
+            })
+            .catch((error) => {
+                console.error("Erro ao deslogar:", error);
+            });
+    };
+
+    return (
+        <>
+            <Navbar />
+            <div className="mt-72 relative">
+                {user && (
+                    <button
+                        onClick={handleLogout}
+                        className="absolute top-4 -mt-52 right-4 bg-red-500 text-white px-4 py-2 rounded-full hover:bg-red-600"
+                    >
+                        Deslogar
+                    </button>
+                )}
+                <h2 className="text-3xl font-bold mb-4 ml-8">
+                    {user ? (
+                        <>Olá, <span className="text-yellow-400">{user.displayName || user.email || "Usuário"}</span></>
+                    ) : (
+                        "Olá, Usuário"
+                    )}
+                </h2>
+                <h3 className="ml-8">Sua sessão de filmes curtidos:</h3>
+                {loading ? (
+                    <div className="text-center">
+                        <p>Carregando filmes...</p>
+                    </div>
+                ) : favoriteContent.length > 0 ? (
+                    <div className="relative z-10">
+                        <MovieRow movies={favoriteContent} />
+                    </div>
+                ) : (
+                    <p className="text-gray-400 mt-2 ml-8">Você ainda não curtiu nenhum filme.</p>
+                )}
+
+                <h3 className="ml-8 mt-8">Sua sessão de séries curtidas:</h3>
+                {loading ? (
+                    <div className="text-center">
+                        <p>Carregando séries...</p>
+                    </div>
+                ) : favoriteSeries.length > 0 ? (
+                    <div className="relative z-10">
+                        <MovieRow movies={favoriteSeries} />
+                    </div>
+                ) : (
+                    <p className="text-gray-400 mt-2 ml-8">Você ainda não curtiu nenhuma série.</p>
+                )}
+            </div>
+        </>
+    );
+};
+
+export default Favorites;
---
+++ pages/Login.jsx
@@ -0,0 +1,187 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router'; 
+
+import { auth } from '@/config/firebaseConfig';
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth';
+
+import Head from 'next/head';
+import Link from 'next/link';
+
+import { FaGoogle } from 'react-icons/fa';
+
+const Auth = () => {
+  const router = useRouter(); 
+  const [user, setUser] = useState(auth.currentUser);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [nickname, setNickname] = useState(''); 
+  const [isSignup, setIsSignup] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    if (user) {
+      router.push('/'); 
+    }
+  }, [user, router]);
+
+  const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      await signInWithPopup(auth, provider);
+      setUser(auth.currentUser);
+    } catch (error) {
+      console.error('Error signing in with Google', error);
+      setError('Erro ao autenticar com Google.');
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      setUser(null);
+    } catch (error) {
+      console.error('Error signing out', error);
+      setError('Erro ao deslogar.');
+    }
+  };
+
+  const handleEmailPasswordAuth = async () => {
+    setError('');
+    try {
+      if (isSignup) {
+        if (!nickname.trim()) {
+          setError('O campo de nickname é obrigatório.');
+          return;
+        }
+
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+        await updateProfile(userCredential.user, {
+          displayName: nickname,
+        });
+
+        setUser(auth.currentUser);
+      } else {
+        await signInWithEmailAndPassword(auth, email, password);
+        setUser(auth.currentUser);
+      }
+    } catch (error) {
+      console.error('Error with email and password', error);
+      setError('Erro ao autenticar com e-mail e senha.');
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <title>XerifeTV - Login</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <div className="mt-32 max-w-md mx-auto p-7 rounded shadow-lg bg-stone-900">
+        {user ? (
+          <div>
+            <p className="text-white mb-5">Olá, {user.displayName || user.email}</p>
+            <button
+              onClick={handleSignOut}
+              className="mt-2 px-4 py-2 bg-red-600 text-black rounded hover:bg-red-700 transition"
+            >
+              Sair
+            </button>
+            <Link
+              href="/"
+              className="mt-2 px-4 py-2 bg-yellow-400 text-black rounded hover:bg-yellow-500 transition inline-block ml-2"
+            >
+              Home
+            </Link>
+          </div>
+        ) : (
+          <div>
+            {error && <p className="text-red-500">{error}</p>}
+
+            <div className="flex justify-center mb-12">
+              <img
+                height="100"
+                width="150"
+                src="https://pt.boxcritters.wiki/images/3/38/Chap%C3%A9u_Pirata_%C3%ADcone.png"
+                alt="Logo"
+              />
+            </div>
+
+            {isSignup && (
+              <div className="mb-4">
+                <p className="text-center mb-2 text-white">Nickname:</p>
+                <input
+                  type="text"
+                  className="w-full px-4 py-2 text-white bg-stone-950 rounded"
+                  placeholder="..."
+                  value={nickname}
+                  onChange={(e) => setNickname(e.target.value)}
+                />
+              </div>
+            )}
+
+            <div className="mb-4">
+              <p className="text-center mb-2 text-white">Email:</p>
+              <input
+                type="email"
+                className="w-full px-4 py-2 text-white bg-stone-950 rounded mb-4"
+                placeholder="..."
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <p className="text-center mb-2 text-white">Senha:</p>
+              <input
+                type="password"
+                className="w-full px-4 py-2 text-white bg-stone-950 rounded mb-8"
+                placeholder="..."
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="flex justify-center mb-8">
+              <button
+                onClick={signInWithGoogle}
+                className="flex items-center px-6 py-3 text-white bg-red-600 rounded-lg hover:bg-red-600/90 transition transform hover:scale-105 shadow-md"
+              >
+                <FaGoogle className="mr-3 text-xl" />
+                Entrar com Google
+              </button>
+            </div>
+
+            <div className="flex justify-center mb-10">
+              <button
+                onClick={handleEmailPasswordAuth}
+                className="flex items-center px-6 py-3 text-white bg-red-600 rounded-lg hover:bg-red-600/90 transition transform hover:scale-105 shadow-md"
+              >
+                {isSignup ? 'Cadastrar' : 'Entrar'}
+              </button>
+            </div>
+
+            <div className="mt-4 mb-3 text-center">
+              <p>
+                {isSignup ? 'Já tem uma conta?' : 'Ainda não tem uma conta?'}{' '}
+                <button
+                  onClick={() => setIsSignup(!isSignup)}
+                  className="text-yellow-400 underline"
+                >
+                  {isSignup ? 'Fazer login' : 'Criar conta'}
+                </button>
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Auth;
